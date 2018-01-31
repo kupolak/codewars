30fdb352939020efc8561c50de4877a673b11248
Jakub Polak <jakub.polak.vz@gmail.com>
2018-01-31 13:54:30 +0100

    Online RPG: player to qualifying stage?

---
+++ Ruby/8 KYU/Online RPG: player to qualifying stage?.rb
@@ -0,0 +1,7 @@
+def playerRankUp(points)
+  if points >= 100
+    return "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up."
+  else
+    return false
+  end
+end
