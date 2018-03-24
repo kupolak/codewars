de065e447128895b68a561a92a2f8d8a764133e6
Jakub Polak <jakub.polak.vz@gmail.com>
2018-03-24 16:51:47 +0100

    Switch it Up!

---
+++ Ruby/8 KYU/Switch it Up!.rb
@@ -0,0 +1,24 @@
+def switch_it_up(number)
+  case number
+  when 0
+    'Zero'
+  when 1
+    'One'
+  when 2
+    'Two'
+  when 3
+    'Three'
+  when 4
+    'Four'
+  when 5
+    'Five'
+  when 6
+    'Six'
+  when 7
+    'Seven'
+  when 8
+    'Eight'
+  when 9
+    'Nine'
+  end
+end
