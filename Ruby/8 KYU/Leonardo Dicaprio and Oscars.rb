67c59786a6ad08ed8a750fdb23227b87c833c7a8
Jakub Polak <jakub.polak.vz@gmail.com>
2018-03-26 17:54:32 +0200

    Leonardo Dicaprio and Oscars

---
+++ Ruby/8 KYU/Leonardo Dicaprio and Oscars.rb
@@ -0,0 +1,14 @@
+def leo(oscar)
+  if oscar == 88
+    return "Leo finally won the oscar! Leo is happy"
+  end
+  if oscar == 86
+    return "Not even for Wolf of wallstreet?!"
+  end
+  if oscar < 88 && oscar != 86
+    return "When will you give Leo an Oscar?"
+  end
+  if oscar > 88
+    return "Leo got one already!"
+  end
+end
