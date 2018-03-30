21f36456c3d397b5a33377cc7fa794b794a57619
Jakub Polak <jakub.polak.vz@gmail.com>
2018-03-30 18:02:22 +0200

    Define a card suit

---
+++ Ruby/8 KYU/Define a card suit.rb
@@ -0,0 +1,8 @@
+def define_suit(card)
+  if card[-1] == "C" then'clubs'
+  elsif card[-1] == "C" then'clubs'
+  elsif card[-1] == "S" then 'spades'
+  elsif card[-1] == "D" then 'diamonds'
+  elsif card[-1] == "H" then 'hearts'
+  end
+end
