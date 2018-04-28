847428e28566bc6d5ede32f60199808ad117ff6a
Jakub Polak <jakub.polak.vz@gmail.com>
2018-04-28 18:56:43 +0200

    Training JS #8: Conditional statement--switch

---
+++ Javascript/8 KYU/Training JS #8: Conditional statement--switch.js
@@ -0,0 +1,15 @@
+function howManydays(month){
+    var days;
+    switch (month){
+        case 2:
+            days = 28;
+            break;
+        case 4: case 6: case 9: case 11:
+            days = 30;
+            break;
+        default:
+            days = 31;
+            break;
+    }
+    return days;
+}
