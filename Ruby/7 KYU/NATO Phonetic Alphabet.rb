d6e3adad44d21d20d8d9ac7eed484a827fb313cb
Jakub Polak <jakub.polak.vz@gmail.com>
2018-05-14 19:11:42 +0200

    NATO Phonetic Alphabet

---
+++ Ruby/7 KYU/NATO Phonetic Alphabet.rb
@@ -0,0 +1,19 @@
+def nato(word)
+  letters =  {
+      "A"=> "Alpha",  "B"=> "Bravo",   "C"=> "Charlie",
+      "D"=> "Delta",  "E"=> "Echo",    "F"=> "Foxtrot",
+      "G"=> "Golf",   "H"=> "Hotel",   "I"=> "India",
+      "J"=> "Juliett","K"=> "Kilo",    "L"=> "Lima",
+      "M"=> "Mike",   "N"=> "November","O"=> "Oscar",
+      "P"=> "Papa",   "Q"=> "Quebec",  "R"=> "Romeo",
+      "S"=> "Sierra", "T"=> "Tango",   "U"=> "Uniform",
+      "V"=> "Victor", "W"=> "Whiskey", "X"=> "X-ray",
+      "Y"=> "Yankee", "Z"=> "Zulu"
+  }
+  word = word.split("").map! {|w| w.upcase!}
+  array = []
+  word.each do |w|
+    array << letters[w]
+  end
+  return array.join(" ")
+end
