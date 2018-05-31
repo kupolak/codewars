e8cd107078a57f5ad41e91fcedad0d06fe8ed5cd
Jakub Polak <jakub.polak.vz@gmail.com>
2018-06-01 00:09:13 +0200

    For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre

---
+++ Ruby/8 KYU/For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre.rb
@@ -0,0 +1,8 @@
+def quote(fighter)
+  fighter.downcase!
+  if fighter == "george saint pierre"
+    "I am not impressed by your performance."
+  else
+    "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+  end
+end
