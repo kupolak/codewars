728998a24b1cf75cc405a3870a1376e9f9cb2ac4
Jakub Polak <jakub.polak.vz@gmail.com>
2018-06-23 00:33:43 +0200

    Jenny's secret message

---
+++ Java/8 KYU/Jenny's secret message.java
@@ -0,0 +1,9 @@
+public class Greeter {
+  public static String greet(String name) {
+    if(name.equals("Johnny")){
+      return "Hello, my love!";
+    } else {
+      return String.format("Hello, %s!", name);
+    }
+  }
+}
