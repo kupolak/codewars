6acaca21803b78f50627648cc1cc894dfa3d09cd
Jakub Polak <jakub.polak.vz@gmail.com>
2018-06-25 00:36:57 +0200

    Are You Playing Banjo?

---
+++ Javascript/8 KYU/Are You Playing Banjo?.js
@@ -0,0 +1,7 @@
+function areYouPlayingBanjo(name) {
+    if(name[0] == 'R' || name[0] == 'r') {
+        return name + " plays banjo"
+    } else {
+        return name + " does not play banjo"
+    }
+}
--- Python/8 KYU/Are You Playing Banjo?.py
+++ Python/8 KYU/Are You Playing Banjo?.py
@@ -2,4 +2,4 @@ def areYouPlayingBanjo(name):
     if name[0] == 'R' or name[0] == 'r':
         return name + " plays banjo"
     else:
-        return name + " does not play banjo" 
+        return name + " does not play banjo"
