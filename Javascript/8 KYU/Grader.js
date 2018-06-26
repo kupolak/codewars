3ce0b5f22456540b3c6acbcb3007d376f494f303
Jakub Polak <jakub.polak.vz@gmail.com>
2018-06-27 00:40:24 +0200

    Grader

---
+++ Javascript/8 KYU/Grader.js
@@ -0,0 +1,13 @@
+function grader(score) {
+    if(score < 0.6 || score > 1) {
+        return 'F'
+    } if(score >= 0.9) {
+        return 'A'
+    } if(score >= 0.8) {
+        return 'B'
+    } if(score >= 0.7) {
+        return 'C'
+    } if(score >= 0.6) {
+        return 'D'
+    }
+}
