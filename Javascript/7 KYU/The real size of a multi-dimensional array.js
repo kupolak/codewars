578e351b14d5eae956a9e044056eda2814d3bed8
Jakub Polak <jakub.polak.vz@gmail.com>
2018-07-06 12:53:16 +0200

    The real size of a multi-dimensional array

---
+++ Javascript/7 KYU/The real size of a multi-dimensional array.js
@@ -0,0 +1,11 @@
+function realSize(arrays) {
+    return arrays.flat(5).length;
+}
+
+Object.defineProperty(Array.prototype, 'flat', {
+    value: function(depth = 1) {
+        return this.reduce(function (flat, toFlatten) {
+            return flat.concat((Array.isArray(toFlatten) && (depth-1)) ? toFlatten.flat(depth-1) : toFlatten);
+        }, []);
+    }
+});
