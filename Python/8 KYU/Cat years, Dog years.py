cb76b9966423bb03f9fb937783dc76ff5b61d168
Jakub Polak <jakub.polak.vz@gmail.com>
2018-09-04 20:42:54 +0200

    Cat years, Dog years

---
+++ Python/8 KYU/Cat years, Dog years.py
@@ -0,0 +1,11 @@
+def human_years_cat_years_dog_years(human_years):
+    if human_years == 1:
+        cat_years = 15
+        dog_years = 15
+    elif human_years == 2:
+        cat_years = 24
+        dog_years = 24
+    elif human_years > 2:
+        cat_years = 16 + (4 * human_years)
+        dog_years = 14 + (5 * human_years)
+    return [human_years,cat_years,dog_years]
