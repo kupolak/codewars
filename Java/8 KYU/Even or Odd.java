7f324ce69d56606459e73730968a390b95ef97d0
Jakub Polak <jakub.polak.vz@gmail.com>
2018-11-05 21:38:34 +0100

    Even or Odd

---
+++ Java/8 KYU/Even or Odd.java
@@ -0,0 +1,9 @@
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        if(number % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
+}
