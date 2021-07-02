8b3b78e5d8cff74a9abf30a05a85b170e79ce510
Jakub Polak <kupolak@users.noreply.github.com>
2021-07-02 14:23:33 +0200

    Create Image host filename generator.js

---
+++ Javascript/6 KYU/Image host filename generator.js
@@ -0,0 +1,8 @@
+function generateName(){
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 6; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  return text;
+}
