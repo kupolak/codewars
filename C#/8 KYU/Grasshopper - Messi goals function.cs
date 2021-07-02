dba1c96549e196e0eb85b5e8f7ada06168f59837
Jakub Polak <kupolak@users.noreply.github.com>
2021-07-02 16:58:59 +0200

    Create Grasshopper - Messi goals function.cs

---
+++ C#/8 KYU/Grasshopper - Messi goals function.cs
@@ -0,0 +1,7 @@
+public class Kata
+{
+  public static int GetGoals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals)
+  {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  }
+}
