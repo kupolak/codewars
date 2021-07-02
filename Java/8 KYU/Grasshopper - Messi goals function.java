18b4ac5ac892efc687a0edf76d6db8ec10d7a8ce
Jakub Polak <kupolak@users.noreply.github.com>
2021-07-02 16:59:59 +0200

    Create Grasshopper - Messi goals function.java

---
+++ Java/8 KYU/Grasshopper - Messi goals function.java
@@ -0,0 +1,5 @@
+public class Goals {
+    public static int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+       return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+    }
+}
