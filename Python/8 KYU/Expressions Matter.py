65657d9640e39704d6cd7d7fe8c81707d39883b9
Jakub Polak <kupolak@users.noreply.github.com>
2021-07-02 17:01:39 +0200

    Create Expressions Matter.py

---
+++ Python/8 KYU/Expressions Matter.py
@@ -0,0 +1,7 @@
+def expression_matter(a, b, c):
+    one = a * (b + c)
+    two = a * b * c
+    three = a + b * c
+    four = (a + b) * c
+    five = a + b + c
+    return max([one, two, three, four, five])
