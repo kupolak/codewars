b496cc8045603631278a7bba1f7a857f963a79b8
Jakub Polak <kupolak@users.noreply.github.com>
2021-07-02 16:06:16 +0200

    Update Vowel Count.py

--- Python/7 KYU/Vowel Count.py
+++ Python/7 KYU/Vowel Count.py
@@ -1,3 +1,8 @@
-def getCount(inputStr)
-  inputStr.count("aeiou")
-end
+def getCount(inputStr):
+    num_vowels = 0
+    l = list(inputStr)
+    for i in l:
+        if i == 'a' or i == 'e' or i == 'i' or i == 'o' or  i == 'u':
+            num_vowels += 1
+    
+    return num_vowels
