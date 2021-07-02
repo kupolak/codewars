38ae83b0222429d1111d3e7b0c7121820bbaa4e0
Jakub Polak <kupolak@users.noreply.github.com>
2021-07-02 15:04:19 +0200

    Create Adding Big Numbers.js

---
+++ Javascript/4 KYU/Adding Big Numbers.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+    let result = '';
+    let c = 0;
+    a = a.split('');
+    b = b.split('');
+    while (a.length || b.length || c) {
+        c += ~~a.pop() + ~~b.pop();
+        result = c % 10 + result;
+        c = c > 9
+    }
+    return result
+}
