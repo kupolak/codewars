1e31a2d88e3e698b65f8b59d7187ad9e135e6ddc
Jakub Polak <kupolak@users.noreply.github.com>
2021-07-03 12:31:11 +0200

    Create Drink about.js

---
+++ Javascript/8 KYU/Drink about.js
@@ -0,0 +1,11 @@
+function peopleWithAgeDrink(old) {
+    if(old < 14) {
+        return 'drink toddy'
+    } else if(old < 18) {
+        return 'drink coke'
+    } else if(old < 21) {
+        return 'drink beer'
+    } else {
+        return 'drink whisky'
+    }
+}
